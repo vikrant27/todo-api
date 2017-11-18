cea39fbaf9279182713d0b5c3f3b3bce1a636018
Vikrant Mahajan <vikrantm93@gmail.com>
2017-11-18 19:44:31 +0530

    Add delete

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,44 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) =>{
+  if (err) {
+  	return console.log('Unable to connect to MongoDb server');
+  }
+  console.log('Connected to MongoDb server');
+  
+  //deleteMany
+
+  // db.collection('Todos').deleteMany({text: 'Eat Lunch'}).then((result) =>{
+  //    console.log(result);
+  // });
+
+  //deleteOne
+
+  // db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) =>{
+  //     console.log(result);
+  // });
+
+  //findOneAndDelete
+
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) =>{
+  //     console.log(result);
+  // });
+
+
+  // db.collection('Users').deleteMany({name: 'mike'}).then((result) =>{
+  //     console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id:new ObjectID('5a10345a2e2a8b02f2061945')}).then((result) => {
+      console.log(JSON.stringify(result, undefined,2));
+  });
+
+  
+  //db.close();
+
+});
