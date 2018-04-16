306f1646149cb59a4a00d697360dd44f34fb4927
Vikrant Mahajan <vikrantm93@gmail.com>
2018-04-16 17:42:50 +0530

    Add GEt /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express =require('express');
 var bodyParser =require('body-parser');
 
+var {ObjectID} =require('mongodb');
+
 var {mongoose} =require('./db/mongoose');
 var {Todo} =require('./models/todo');
 var {User} =require('./models/user');
@@ -30,6 +32,31 @@ app.get('/todos', (req,res) =>{
 	},(e)=>{
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id',(req,res)=>{
+	var id = req.params.id;
+
+	//Valid id using isValid
+	//404- send back empty send
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) =>{
+		if(!todo){
+			return res.status(404).send();
+		}
+
+		res.send({todo})
+	}).catch((e)=>{
+		res.status(404).send();
+	});
+
+	
+
+	
 });
 
 app.listen(3000, () =>{
