96f876638bd38d779a95315070934f9a950f5338
Vikrant Mahajan <vikrantm93@gmail.com>
2018-04-17 15:06:26 +0530

    Setup separate dev and Test env

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+require('./config/config')
+
+
 const express =require('express');
 const bodyParser =require('body-parser');
 const _= require('lodash');
@@ -11,7 +14,7 @@ const {User} =require('./models/user');
 
 var app =express();
 
-const port =process.env.PORT || 3000;
+const port =process.env.PORT;
 
 app.use(bodyParser.json());
 
