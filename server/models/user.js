f4f2f051a446e1ee14fff888514c90fa478e6eb5
Vikrant Mahajan <vikrantm93@gmail.com>
2018-04-25 20:52:39 +0530

    hash user password

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const mongoose =require('mongoose');
 const validator=require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email:{
@@ -71,6 +72,20 @@ UserSchema.statics.findByToken=function(token){
   });
 
 };
+
+UserSchema.pre('save', function(next){
+  var user = this;
+
+  if(user.isModified('password')){
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(user.password, salt);
+     user.password =hash;
+     next();
+  }else{
+    next();
+  }
+});
 
 var User =mongoose.model('User',UserSchema);
 
