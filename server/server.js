15c8f15754575cded1b0d2c8017225841f8cff8f
Vikrant Mahajan <vikrantm93@gmail.com>
2018-04-26 17:27:40 +0530

    Private Route

--- server/server.js
+++ server/server.js
@@ -19,10 +19,11 @@ const port =process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res) => {
+app.post('/todos',authenticate,(req,res) => {
    
    var todo =new Todo({
-   	 text: req.body.text
+   	 text: req.body.text,
+   	 _creator: req.user._id
    });
 
    todo.save().then((doc) => {
@@ -32,16 +33,19 @@ app.post('/todos',(req,res) => {
    });
 });
 
-app.get('/todos', (req,res) =>{
-	Todo.find().then((todos)=>{
-		res.send({todos});
+app.get('/todos', authenticate,(req,res) =>{
+	Todo.find({
+		_creator:req.user._id
+	}).then((todos)=>{
+		console.log(todos);
+		res.send({success:'1',message:'success',todos});
 
 	},(e)=>{
 		res.status(400).send(e);
 	});
 });
 
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
 	var id = req.params.id;
 
 	//Valid id using isValid
@@ -51,7 +55,10 @@ app.get('/todos/:id',(req,res)=>{
 		return res.status(404).send();
 	}
 
-	Todo.findById(id).then((todo) =>{
+	Todo.findOne({
+		_id:id,
+		_creator:req.user._id
+	}).then((todo) =>{
 		if(!todo){
 			return res.status(404).send();
 		}
@@ -62,14 +69,17 @@ app.get('/todos/:id',(req,res)=>{
 	});	
 });
 
-app.delete('/todos/:id',(req,res) =>{
+app.delete('/todos/:id',authenticate,(req,res) =>{
 	var id= req.params.id;
 
 	if(!ObjectID.isValid(id)){
 		return res.status(404).send();
 	}
 
-	Todo.findByIdAndRemove(id).then((todo) =>{
+	Todo.findOneAndRemove({
+		_id:id,
+		_creator:req.user._id
+	}).then((todo) =>{
 		if(!todo){
 			return res.status(404).send();
 		}
@@ -80,7 +90,7 @@ app.delete('/todos/:id',(req,res) =>{
 	});	
 });
 
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id',authenticate,(req,res)=>{
 	var id = req.params.id;
 	var body = _.pick(req.body,['text','completed']);
 
@@ -95,7 +105,9 @@ app.patch('/todos/:id',(req,res)=>{
 		body.completedAt = null;
 	}
 
-	Todo.findByIdAndUpdate(id,{$set:body
+	Todo.findOneAndUpdate({
+		_id:id,_creator:req.user._id
+	},{$set:body
 	},{new:true}).then((todo)=>{
 
 		if(!todo){
